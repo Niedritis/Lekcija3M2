7c54695ef9d3cb64f858961a1b8e2eb3af44bf59
Niedritis <niedritis@inbox.lv>
2022-04-24 21:17:32 +0300

    ddd

---
+++ M2/M2/Car.cs
@@ -0,0 +1,39 @@
+﻿namespace M2
+{
+    internal class Car
+    {
+        public string Brand { get; set; }
+        public string NumberPlate { get; set; }
+        public double Speed { get; set; }
+
+        public void StartToDrive()
+        {
+            Speed = 15;
+            Console.WriteLine("Sakam braukšanu");
+        }
+
+        public double GoFaster()
+        {
+            Speed += 10;
+            Console.WriteLine("Ātrums - " + Speed);
+            return Speed;
+        }
+
+        public void Beep()
+        {
+            Console.WriteLine("BEEP");
+        }
+        public double SlowDown()
+        {
+            Speed -= 10;
+            Console.WriteLine("Ātrums - " + Speed);
+            return Speed;
+        }
+
+        public void Stop()
+        {
+            Speed = 0;
+            Console.WriteLine("Apstāšanās");
+        }
+    }
+}
---
+++ M2/M2/Program.cs
@@ -0,0 +1,20 @@
+﻿using M2;
+
+Car car1 = new Car();
+
+
+car1.Brand = "Toyota";
+car1.NumberPlate = "4545";
+car1.Speed = 20;
+
+
+string carName = car1.Brand;
+string carNumberPlate = car1.NumberPlate;
+double carSpeed = car1.Speed;
+
+Console.WriteLine($"Auto {carName} numurs {carNumberPlate}");
+
+car1.StartToDrive();
+car1.GoFaster();
+car1.Beep();
+car1.SlowDown();
